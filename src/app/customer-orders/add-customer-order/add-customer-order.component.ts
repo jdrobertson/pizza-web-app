091513be212ecb34341ad5f4fe6665d54147d19d
Joel Robertson <joel@joelrobertson.net>
2022-03-04 15:51:41 +1300

    add customer order component

---
+++ src/app/customer-orders/add-customer-order/add-customer-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-customer-order',
+  templateUrl: './add-customer-order.component.html',
+  styleUrls: ['./add-customer-order.component.scss']
+})
+export class AddCustomerOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/customer-orders/customer-orders-routing.module.ts
+++ src/app/customer-orders/customer-orders-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCustomerOrderComponent } from './add-customer-order/add-customer-order.component';
 import { CustomerOrdersComponent } from './customer-orders.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: CustomerOrdersComponent,
   },
+  {
+    path: 'add',
+    component: AddCustomerOrderComponent,
+  }
 ];
 
 @NgModule({
