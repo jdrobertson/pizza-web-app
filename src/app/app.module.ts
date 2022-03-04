d287bfbe1844bc7b8228624f6a717b290f304509
Joel Robertson <joel@joelrobertson.net>
2022-03-04 15:51:24 +1300

    add customer order service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/customer-orders/shared/customer-order.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { CustomerOrder } from './customer-order.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerOrderService {
+
+  apiUrl = 'http://localhost:8080/customerorders';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getCustomerOrders() {
+    return this.httpClient.get<CustomerOrder[]>(this.apiUrl);
+  }
+}
